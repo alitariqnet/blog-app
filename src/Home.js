471caedc74c315adb3f934a22f6fdc3d79487080
Ali Tariq <alibabapk@aol.com>
2024-07-14 17:08:17 +0500

    code update

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,6 @@ const Home = () => {
     return ( <div className="home">
         <h2>Homepage</h2>
         <BlogList blogs={blogs} title="All blogs!"/>
-        <BlogList blogs={blogs.filter((blog) => { return blog.id === 'alibaba' })} title="Ali's blogs!"/>
     </div> );
 }
  
